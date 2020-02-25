d7d302a6c2abbc617489a3acf93745ed7c660c5c
coydogbjj <blitzzxc@gmail.com>
2020-02-25 00:14:56 -0600

    Create Kids Reset.ps1

---
+++ Kids Reset.ps1
@@ -0,0 +1,64 @@
+#######################################  MODIFY AS NEEDED BELOW  ###########################################################################
+                    
+#Local Account to Modify
+$account = "Kids"
+
+#Remote PC1 ComputerName, Admin Account, Account to modify, Admin Account, Credentials
+$RPC1 = "BOYSPC"
+$RPC1admin = "DADMIN"
+$RPC1pass = "Get-Content F:\Utilities\Kids-Pass\Creds\BOYSPC_creds.securestring | ConvertTo-SecureString"
+$RPC1account = "boys"
+                 
+#Password List File Name ENSURE IT RESIDES IN CREDS DIRECTORY ($listdir)
+$listfile = "pass-list.txt"
+
+#Directory for Creds
+$listdir = "F:\Utilities\Kids-Pass\Creds"
+                   
+#Gmail SMTP Settings
+$emailserver = "smtp.gmail.com"
+$emailport = "587"
+                   
+#From & Cc Email Address
+$email1 = "BlitzzXC@gmail.com"
+                    
+#To Email Address
+$email2 = "Samanthamccoy88@gmail.com"
+
+#Lock Local System
+$xCmdString = {C:\Windows\System32\rundll32.exe user32.dll,LockWorkStation}
+                    
+#######################################  DO NOT MODIFY BELOW  ###########################################################################
+                    
+#Date Calculations
+$date = Get-Date
+$tomorrow = (($date).AddDays(1))
+$tomorrowmonth = $tomorrow.Month
+                    
+#Date Formatting
+$weekdaypart = $tomorrow.DayofWeek
+$monthpart = $tomorrowmonth | %{(Get-Culture).DateTimeFormat.GetMonthName($_)}
+$daypart = $tomorrow.Day
+                                    
+#Static Variables
+$newpassword = Get-Content $listdir\$listfile | Get-Random
+$securepass = Get-Content "$listdir\BlitzzXC@gmail.com.securestring" | ConvertTo-SecureString
+$emailcreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $email1,$securepass
+                    
+#Email Template
+$body = "$weekdaypart, $monthpart $daypart - The $account password has been changed to: $newpassword"
+$subject = "$account Password Change"
+    
+#######################################  EXECUTION  ###########################################################################
+#Change pass on local system
+PowerShell -NoProfile -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile net user $account "$newpassword"' -Verb RunAs}" 
+#Change pass on remote system
+F:\Utilities\Kids-Pass\Creds\pspasswd64.exe \\$RPC1 -u $RPC1admin -p $RPC1pass -nobanner $RPC1account $newpassword
+#Email new pass
+Send-MailMessage -From $email1 -To $email2 -Cc $email1 -Credential $emailcreds -SmtpServer $emailserver -Port $emailport -UseSsl -Subject $subject -Body $body   
+#reboot remote system... kids get wreked
+F:\Utilities\Kids-Pass\Creds\psshutdown.exe -r -u $RPC1admin -p $RPC1pass \\$RPC1
+#Sleep for timing
+Start-Sleep -Seconds 20
+#Lock local system
+Invoke-Command $xCmdString
